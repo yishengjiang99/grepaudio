a23e096143af6d4f422ab6d2ad3b764f221a2859
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-04-10 02:00:07 -0700

    dd

--- http.js
+++ http.js
@@ -16,11 +16,13 @@ app.use(function (req,res,next)
   res.header("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT");
   next();
 });
+
 app.get("/yt", async (req,res) =>{
   const query = req.params.q;
 
   const youtube_api_key = process.env.GOOGLE_KEY_OMG;  
-  const url =`https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=10&q=${query}&key=${youtube_api_key}`
+  const n = req.params.n || 1;
+  const url =`https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=${n}&q=${query}&key=${youtube_api_key}`
   const request = require("request");
 
   request({
@@ -60,10 +62,10 @@ app.get("/yt", async (req,res) =>{
 app.use("/(:vid).mp3",async (req,res,next) =>
 {
   try {
-    vid = req.params
+    vid = req.params.vid;
+
     FFmpeg.setFfmpegPath(ffmpegPath);
     const video = ytdl(vid,{ audioFormat: 'mp3' });
-
     const ffmpeg = new FFmpeg(video);
     process.nextTick(() => ffmpeg.format('mp3').pipe(new PassThrough()).pipe(res))
   } catch (e) {
